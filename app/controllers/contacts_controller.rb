1ee15461ee28037bad4dfcddef1abd70499b474f
shohei <shohei.miyagi@devenues.com>
2023-08-08 18:15:09 +0900

    html修正

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,9 +3,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
-  # 確認画面を作成する場合はこのような記述になるかと思います。
-  # newアクションから入力内容を受け取り、
-  # 送信ボタンを押されたらcreateアクションを実行します。
+  # 確認画面
   def confirm
     @contact = Contact.new(contact_params)
     if @contact.invalid?
@@ -13,23 +11,14 @@ class ContactsController < ApplicationController
     end
   end
 
-  # 入力内容に誤りがあった場合、
-  # 入力内容を保持したまま前のページに戻るのが当たり前になっているかと思いますが、
-  # backアクションを定義することで可能となります。
+  # 入力内容に誤りがあった場合
   def back
     @contact = Contact.new(contact_params)
     render :new
   end
 
-  # 実際に送信するアクションになります。
-  # ここで初めて入力内容を保存します。
-  # セキュリティーのためにも一定時間で入力内容の削除を行ってもいいかもしれません。
   def create
     @contact = Contact.new(contact_params)
-
-    puts '=================================='
-    p @contact.subject
-    puts '=================================='
     if @contact.save
       ContactMailer.send_mail(@contact).deliver_now
       redirect_to done_path
@@ -38,9 +27,8 @@ class ContactsController < ApplicationController
     end
   end
 
-  # 送信完了画面を使用する場合お使いください。
   def done
-    # @contact = Contact.new(contact_params)
+
   end
 
   private
