ed4a26460663385dfa62616f20bf6d2618c595b8
shohei <shohei.miyagi@devenues.com>
2023-08-08 17:38:42 +0900

    バリデーション設定

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,12 @@
 class Contact < ApplicationRecord
   enum subject: { estimate: 1, question: 2, introduction: 3 }
+
+  validates :name, presence: true, length: { maximum: 20 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 30 },
+                    format: { with: VALID_EMAIL_REGEX }
+
+  VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
+  validates :phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }
 end
